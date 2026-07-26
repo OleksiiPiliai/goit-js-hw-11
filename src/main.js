ff2490b3039efa13f3c237c42e42df3744e6eb1e
Oleksii Piliai <alekseivpiliai@gmail.com>
2026-07-26 23:10:23 +0200

    start4

--- src/main.js
+++ src/main.js
@@ -1,57 +1,55 @@
-'use strict';
-import { fetchImages } from './js/pixabay-api';
-import { renderImages, setupImageLoadHandlers } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/render-functions';
 
 const form = document.querySelector('.form');
-const container = document.querySelector('.gallery');
-let lightbox = null;
 
 form.addEventListener('submit', handleSearch);
 
 function handleSearch(event) {
   event.preventDefault();
-  const { textRow } = event.target.elements;
 
-  if (textRow.value.trim() === '') {
+  const currentForm = event.currentTarget;
+  const query = currentForm.elements['search-text'].value.trim();
+
+  if (!query) {
     iziToast.warning({
-      title: 'Caution',
-      message: 'The search field cannot be empty.',
-      color: 'red',
+      message: 'Please enter a search query.',
+      position: 'topRight',
     });
     return;
   }
 
-  fetchImages(textRow.value)
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.warning({
-          title: 'Caution',
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(images => {
+      if (images.length === 0) {
+        iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
-          color: 'yellow',
+          position: 'topRight',
         });
-      } else {
-        container.innerHTML = renderImages(data.hits);
-
-        setupImageLoadHandlers(container);
-
-        if (lightbox) {
-          lightbox.refresh();
-        } else {
-          lightbox = new SimpleLightbox('.gallery .galleries a', {
-            captionsData: 'alt',
-            captionDelay: 250,
-          });
-        }
+        return;
       }
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
-    });
 
-  form.reset();
+      createGallery(images);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      currentForm.reset();
+    });
 }
