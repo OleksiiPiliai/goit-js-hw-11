06cddf822a16573b601f7b863c8fe92a66efd379
Oleksii Piliai <alekseivpiliai@gmail.com>
2026-07-26 23:08:34 +0200

    start3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,18 @@
-`use strict`;
+import axios from 'axios';
 
-const API_KEY = '46858966-ba511c0c272bf1bf47f1d2839';
+const API_KEY = '55965414-3154a5ab5237f7ce8780065e8';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function fetchImages(query) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
+export async function getImagesByQuery(query) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
   });
-  return fetch(`${BASE_URL}?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  });
+
+  return response.data.hits;
 }
